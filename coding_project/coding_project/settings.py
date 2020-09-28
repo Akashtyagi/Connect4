4e4beed723da31bd5702888326fcc801150476f1
Akashtyagi08 <akashtyagi@qainfotech.com>
2020-09-28 12:15:44 +0530

    Library support

--- coding_project/coding_project/settings.py
+++ coding_project/coding_project/settings.py
@@ -14,15 +14,14 @@ from pathlib import Path
 import os
 import dotenv 
 import django_heroku
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+print(BASE_DIR)
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
